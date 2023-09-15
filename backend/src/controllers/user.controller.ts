c0eec2c5a003ac143e08f562e8ff9937a53cdad2
Vv97 <VishalVarma537@gmail.com>
2023-09-15 16:45:06 +0530

    user login  api complete

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import { createUser, findOneUser } from "../services/users.service";
-import { hashPassword } from "../helper/bcryptHelper";
+import { compareHashPassword, hashPassword } from "../helper/bcryptHelper";
 import jwt from "jsonwebtoken";
 
 const secret_key = "0c23307b0d326d12b5f3c349a4c8d0";
@@ -31,6 +31,45 @@ export const userRegisterController = async (req: Request, res: Response) => {
     }
   } catch (error) {
     console.log("Error while registering", error);
+    res.status(400).send(error);
+  }
+};
+
+export const userLoginController = async (req: Request, res: Response) => {
+  try {
+    let { email, password } = req.body;
+    let userExist = await findOneUser({ email });
+
+    if (!userExist) {
+      return res.status(400).send({ message: "user not found." });
+    }
+
+    const hashPassword: string | undefined =
+      userExist?.password?.toString() || "";
+
+    const correctPassword = await compareHashPassword(password, hashPassword);
+
+    if (!correctPassword) {
+      return res.status(404).send({
+        message: "Invalid Password",
+      });
+    }
+
+    const token = await jwt.sign(
+      { _id: userExist?._id?.toString() },
+      secret_key
+    );
+
+    res.status(200).send({
+      message: "Login Successfully",
+      user: {
+        username: userExist?.username,
+        email: userExist?.email,
+      },
+      token,
+    });
+  } catch (error) {
+    console.log("Error while login", error);
     res.status(400).send(error);
   }
 };
