784716cb2066cd990de0ffcec3579d151ff41b0f
Vv97 <VishalVarma537@gmail.com>
2023-09-17 12:27:59 +0530

    minor response message change

--- backend/src/controllers/recipes.controller.ts
+++ backend/src/controllers/recipes.controller.ts
@@ -8,12 +8,12 @@ export const searchRecipesController = async (req: Request, res: Response) => {
     const recipes = await searchRecipes(query?.toString() ?? "");
 
     if (!recipes) {
-      res.status(404).send({ message: "Resource not found." });
+      res.status(404).send({ message: "Recipe not found." });
     }
 
     res.status(200).send(recipes);
   } catch (error: any) {
-    throw new Error(error);
     console.log("Error while fetching recipes", error);
+    throw new Error(error);
   }
 };
