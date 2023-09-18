c98f98588e39ef6d4dc36d2a361c5a6ef7c90acf
Vv97 <VishalVarma537@gmail.com>
2023-09-18 13:20:45 +0530

    added new routes like get post delete

--- backend/src/routes/recipes.routes.ts
+++ backend/src/routes/recipes.routes.ts
@@ -1,9 +1,30 @@
 import { Router } from "express";
-import { searchRecipesController } from "../controllers/recipes.controller";
-
+import {
+  deleteRecipeByID,
+  getSaveRecipesController,
+  getSavedRecipeById,
+  saveRecipesController,
+  searchRecipesController,
+} from "../controllers/recipes.controller";
+import { RequireLogin } from "../middleware/user.middleware";
 const recipesRouter = Router();
+
+//GET : recipes
+recipesRouter.get("/", getSaveRecipesController);
+
+//GET : recpies/:_id
+recipesRouter.get("/:_id", getSavedRecipeById);
 
 //GET : recipes/search
 recipesRouter.get("/search", searchRecipesController);
+
+// middleware to check valid user
+recipesRouter.use(deleteRecipeByID);
+
+//POST : recipes/save
+recipesRouter.post("/save", saveRecipesController);
+
+//DELETE : recipes/delete
+recipesRouter.delete("/delete", deleteRecipeByID);
 
 export default recipesRouter;
